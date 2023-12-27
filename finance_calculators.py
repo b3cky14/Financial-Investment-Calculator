2e99cf5e570289e0eed2a40b7470ee66648508e6
b3cky14 <153016182+b3cky14@users.noreply.github.com>
2023-12-27 12:06:44 +0000

    Update finance_calculators.py

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,7 +1,7 @@
 import math
 
-#use the input function to decide investment or bond
-#use .lower() function so the capitlisation shouldn't effect it being recognised
+# use the input function to decide investment or bond
+# use .lower() function so the capitlisation shouldn't effect it being recognised
 calc = input('''
 Investment - To calculate the amount of interest you'll earn on your investment 
 Bond - To calculate the amount you'll have to pay on a home loan
@@ -9,26 +9,26 @@ Enter either 'Investment' or 'Bond' from the menu above to proceed:
 ''').lower()
 
 
-#if statement if investment was chosen
+# if statement if investment was chosen
 if calc == "investment":
     money = int(input("How much money are you depositing? "))
     rate = int(input("What is your interest rate? "))
     time = int(input("How many years do you plan to invest for? "))
     interest = input("Do you want 'simple' or 'compound' interest? ").lower()
-    #if statment if simple interest was chosen
+    # if statment if simple interest was chosen
     if interest == "simple":
         total = round(money * (1 + (rate/100)*time),2)
         print(f"You will get £{total} after {time} years at an interest rate of {rate}%. ")
-    #elif statement if compound interest was chosen
+    # elif statement if compound interest was chosen
     elif interest == "compound":
         total = round(money * math.pow((1 + (rate/100)),time),2)
         print(f"You will get £{total} after {time} years at an interest rate of {rate}%. ")
-    #else statment if neither simple or compound was chosen
+    # else statment if neither simple or compound was chosen
     else:
         print("You have not chosen simple or compound correctly, please check your spelling. ")
 
 
-#elif statement if bond was chosen
+# elif statement if bond was chosen
 elif calc == "bond":
     value = int(input("What is the present value of your house? "))
     rate = int(input("What is your interest rate? "))
@@ -38,6 +38,6 @@ elif calc == "bond":
     print(f"You will have to repay £{repay} each month for {time} months. ")
 
 
-#else statement if neither investment or bond was chosen
+# else statement if neither investment or bond was chosen
 else:
     print("You have not chosen investment or bond correctly, please check your spelling. ")
